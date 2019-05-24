773cfa048e8d3734576f8547952c84a499a2003c
Carlos <mrfabuland@gmail.com>
2019-05-24 19:42:30 +0200

    Las tuberias se mueven

--- FlappyBird/src/proyectito/FlappyBird.java
+++ FlappyBird/src/proyectito/FlappyBird.java
@@ -21,7 +21,7 @@ public class FlappyBird extends JFrame {
 
 	JPanel panelP;
 	Timer time;
-	JLabel pipe1, pipe2;
+	JLabel pipeUp1, pipeDown1, pipeUp2, pipeDown2, pipeUp3, pipeDown3, suelo;
 
 	public FlappyBird() {
 		Toolkit miPantalla = Toolkit.getDefaultToolkit();
@@ -54,16 +54,41 @@ public class FlappyBird extends JFrame {
 		panelP.setVisible(true);
 		panelP.setLayout(null);
 		getContentPane().add(panelP);
-
-		pipe1 = new JLabel();
-		pipe1.setIcon(new ImageIcon("src\\pics\\pipe1.png"));
-		pipe1.setBounds(526, 300, 100, 450);
-		panelP.add(pipe1);
 		
-		pipe2 = new JLabel();
-		pipe2.setIcon(new ImageIcon("src\\pics\\pipe2.png"));
-		pipe2.setBounds(526, 0, 100, 200);
-		panelP.add(pipe2);
+		suelo = new JLabel();
+		suelo.setIcon(new ImageIcon("src\\pics\\suelo.png"));
+		suelo.setBounds(0, 549, 626, 133);
+		panelP.add(suelo);
+
+		pipeUp1 = new JLabel();
+		pipeUp1.setIcon(new ImageIcon("src\\pics\\pipe1.png"));
+		pipeUp1.setBounds(626, 300, 100, 450);
+		panelP.add(pipeUp1);
+		
+		pipeDown1 = new JLabel();
+		pipeDown1.setIcon(new ImageIcon("src\\pics\\pipe2.png"));
+		pipeDown1.setBounds(626, -200, 100, 450);
+		panelP.add(pipeDown1);
+		
+		pipeUp2 = new JLabel();
+		pipeUp2.setIcon(new ImageIcon("src\\pics\\pipe1.png"));
+		pipeUp2.setBounds(626, 300, 100, 450);
+		panelP.add(pipeUp2);
+		
+		pipeDown2 = new JLabel();
+		pipeDown2.setIcon(new ImageIcon("src\\pics\\pipe2.png"));
+		pipeDown2.setBounds(626, -200, 100, 450);
+		panelP.add(pipeDown2);
+		
+		pipeUp3 = new JLabel();
+		pipeUp3.setIcon(new ImageIcon("src\\pics\\pipe1.png"));
+		pipeUp3.setBounds(626, 300, 100, 450);
+		panelP.add(pipeUp3);
+		
+		pipeDown3 = new JLabel();
+		pipeDown3.setIcon(new ImageIcon("src\\pics\\pipe2.png"));
+		pipeDown3.setBounds(626, -200, 100, 450);
+		panelP.add(pipeDown3);
 
 		empezarJuego();
 	}
@@ -75,27 +100,57 @@ public class FlappyBird extends JFrame {
 	public void empezarJuego() {
 		time = new Timer(10, null);
 		ActionListener listener = new ActionListener() {
-			int movimPipe1 = pipe1.getX();
-			int movimPipe2 = pipe2.getX();
 			int posInicial = 626;
-
+			int movimPipeUp1 = posInicial;
+			int movimPipeDown1 = posInicial;
+			int movimPipeUp2 = posInicial + 300;
+			int movimPipeDown2 = posInicial + 300;
+			int movimPipeUp3 = posInicial + 600;
+			int movimPipeDown3 = posInicial + 600;
 			public void actionPerformed(ActionEvent e) {
-				movimPipe1 -= 2;
-				pipe1.setBounds(movimPipe1, 300, 100, 450);
-				movimPipe2 -= 2;
-				pipe2.setBounds(movimPipe2, 0, 100, 200);
 				
-				if(movimPipe1 == -100) {
-					movimPipe1 = posInicial;
-					movimPipe2 = posInicial;
+				movimPipeUp1 -= 2;
+				pipeUp1.setBounds(movimPipeUp1, 300, 100, 450);
+				movimPipeDown1 -= 2;
+				pipeDown1.setBounds(movimPipeDown1, -250, 100, 450);
+				
+				if(movimPipeUp1 == -300) {
+					movimPipeUp1 = posInicial;
+					movimPipeDown1 = posInicial;
 				}
-				System.out.println(movimPipe1);
-
+				
+				movimPipeUp2 -= 2;
+				pipeUp2.setBounds(movimPipeUp2, 300, 100, 450);
+				movimPipeDown2 -= 2;
+				pipeDown2.setBounds(movimPipeDown2, -250, 100, 450);
+				
+				if(movimPipeUp2 == -300) {
+					movimPipeUp2 = posInicial;
+					movimPipeDown2 = posInicial;
+				}
+				
+				movimPipeUp3 -= 2;
+				pipeUp3.setBounds(movimPipeUp3, 300, 100, 450);
+				movimPipeDown3 -= 2;
+				pipeDown3.setBounds(movimPipeDown3, -250, 100, 450);
+				
+				if(movimPipeUp3 == -300) {
+					movimPipeUp3 = posInicial;
+					movimPipeDown3 = posInicial;
+				}
 			}
 
 		};
 		time.addActionListener(listener);
 		time.start();
+	}
+	
+	public int crearYPipesRandom() {
+		int y1 = 50;
+		int y2 = 526;
+ 		int random = (int)Math.floor(Math.random()*(y1-(y2+1))+(y2));
+		
+		return random;
 	}
 
 }
